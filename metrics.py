17e5c3061f7309dafd4ad1555e79cdafb56ccd5a
nv259 <nguyenvietnhat.03@gmail.com>
2023-11-08 09:38:39 +0700

    add IoU metric

--- metrics.py
+++ metrics.py
@@ -2,7 +2,26 @@ import torch
 import numpy as np
 
 from collections import Counter
-from iou import intersection_over_union as IoU
+
+
+def IoU(boxA, boxB):
+    # determine the (x, y)-coordinates of the intersection rectangle
+	xA = max(boxA[0], boxB[0])
+	yA = max(boxA[1], boxB[1])
+	xB = min(boxA[2], boxB[2])
+	yB = min(boxA[3], boxB[3])
+	# compute the area of intersection rectangle
+	interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
+	# compute the area of both the prediction and ground-truth
+	# rectangles
+	boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
+	boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
+	# compute the intersection over union by taking the intersection
+	# area and dividing it by the sum of prediction + ground-truth
+	# areas - the interesection area
+	iou = interArea / float(boxAArea + boxBArea - interArea)
+	# return the intersection over union value
+	return iou
 
 
 def mean_average_precision(
@@ -49,9 +68,8 @@ def mean_average_precision(
             
             for idx, gt in enumerate(ground_truth_img):
                 iou = IoU(
-                    torch.tensor(detection[3:]),
-                    torch.tensor(gt[3:]),
-                    box_format=box_format,
+                    detection[3:],
+                    gt[3:],
                 )
 
                 if iou > best_iou:

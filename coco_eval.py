3e80894b8dffb0caa0304b675f0745ecccbb7275
nhatnv0378 <nguyenvietnhat.03@gmail.com>
2023-11-10 16:46:04 +0700

    change pycotools -> local

--- coco_eval.py
+++ coco_eval.py
@@ -7,7 +7,7 @@ import pycocotools.mask as mask_util
 import torch
 import utils
 from pycocotools.coco import COCO
-from pycocotools.cocoeval import COCOeval
+from cocoeval import COCOeval
 
 
 class CocoEvaluator:
